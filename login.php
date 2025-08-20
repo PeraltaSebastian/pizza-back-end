a9c058737b727f7170af17c6107a987c86b5282d
Sebastian <peraltasebastian2204@gmail.com>
2025-08-20 19:13:44 -0300

    back-end

--- login.php
+++ login.php
@@ -9,40 +9,45 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
   </head>
-  <body>
-     <?php
-  // Acá va el codigo PHP
-
-  define("USER_REAL","Sebasian");
-  define("PASS_REAL","maria123456");
-
-  //username
-
-  $usr= $_POST['username'];
-
-  //echo "<br>";
-
-  //password
-
-  $pass= $_POST['password'];
-  
-  if ($usr==USER_REAL && $pass=PASS_REAL){
-    echo "<div class='flex flex-col justify-center items-center h-screen'>";
-    echo "<h1 class='font-[Raleway] font-extrabold text-3xl'> ¡¡ Bienvenida/o !!    ". USER_REAL ." </h1>";
-    echo "<h1>". date('d-m-Y'). "</h1>";
-    echo "</div>";
+<body>
+           <?php
+    session_start();
+include_once ("config_login.php"); // ver usar require()
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+try {
+    $pdo = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+$usr = $_POST['username'];
+$pass = $_POST['password'];
+$hashed_pass = hash('sha256', $pass);
+$sql="select * from users where (username=? or email=?) and password=? and active='SI'";
+// Use de sentencias prepared
+// uso de POO- Programacion orientada a objetos
+$stmt=$pdo->prepare($sql);
+$stmt->execute([$usr,$usr,$hashed_pass]);
+$row=$stmt->fetch(PDO::FETCH_ASSOC);
+if(!$row){
+ echo "Los datos ingresados no son validos !";
 }
+ else
+{
+    session_start();
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+    $_SESSION['time'] = date('H:i:s');
+    $_SESSION['username'] = $usr;
+    $_SESSION['logueado']=true;
+    header("location:welcome.php");
 
-  
-  else{
-    echo "<div class='flex flex-col justify-center items-center h-screen'>";
-    echo "<h1 class='font-[Raleway] font-extrabold text-3xl'>  Datos no correspondientes. </h1>";
-    echo "</div>";
-
-  }
-  ?>
-  </body>
-  </html>
+}
+ }
+    ?>
+</body>
+</html>
   
   
 
